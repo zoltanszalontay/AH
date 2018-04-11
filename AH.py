071a10b29db2c51c7ed5bf5e3f1ece11881742bc
Szalontay Zoltán <zoltansz@hotmail.com>
2018-04-11 10:06:40 +0200

    1.0 version: Collects all sensor data and writes to SQL Server

--- AH.py
+++ AH.py
@@ -1,35 +1,166 @@
 #!/usr/bin/python
-# v0.1
+# v1.0
+import datetime as dt
+import logging
 import websocket
-try:
-    import thread
-except ImportError:
-    import _thread as thread
-import time
+import json
+import pyodbc as odbc
+
+sql = {
+    'server':   '', 
+    'database': '',
+    'table':    ''
+    }
+
+euis = {
+    "": "",
+    "": "", 
+    "": "",
+    "": "",
+    "": ""
+    }
+
+class sensor_data:
+    def __init__(self, time, eui, value, unit, sensor_type_id):
+        self.time = time
+        self.eui = eui
+        self.value = float(value)
+        self.unit = unit
+        self.sensor_type_id = sensor_type_id
+
+sdlist = []
+sd_temp = sensor_data 
+sd_power = sensor_data
 
 def on_message(ws, message):
-    print(message)
+    #log.info("on_message()")
 
-def on_error(ws, error):
-    print(error)
+    time = gettime()
+    
+    jsondata = json.loads(message)
+    decode(time, jsondata["EUI"], jsondata["data"])
+    
+    # Insert sensor data to SQL table
+    for i in range(len(sdlist)):
+        log.info("%s | %s, %s, %f, %s, %s" % (jsondata["data"], sdlist[i].time, sdlist[i].eui, round(float(sdlist[i].value), 2), sdlist[i].unit, sdlist[i].sensor_type_id))
+        write_sql(sdlist[i].time, sdlist[i].eui, round(float(sdlist[i].value), 2), sdlist[i].unit, sdlist[i].sensor_type_id)
 
-def on_close(ws):
-    print("### closed ###")
+def decode(time, eui, message):
+    #log.info("decode()")
+    
+    # Reinitialize sensor list
+    del sdlist[:]
+
+    sd = sensor_data("", "", 0.0, "", "")
+    sd.time = time
+    sd.eui = eui
+    
+    # Decode supported sensors only (Rising HF, Digimondo and LS-11x)
+    if (euis[eui] == "Rising HF"):
+        log.info("Rising HF - Temperature | %s | %s ", eui, message)
+        # Decoding temperature
+        temp = int(message[4:6] + message[2:4], 16) * 175.72 / 65536 - 46.85
+        sdlist.append(sensor_data(sd.time, sd.eui, float(temp), "°C", 1))
+
+        # Decode humidity
+        log.info("Rising HF - Humidity | %s | %s ", eui, message)
+        hum = int(message[6:8], 16)*125/2**8 - 6
+        sdlist.append(sensor_data(sd.time, sd.eui, float(hum), "%RH", 2))
+
+        # Decode battery percentage
+        log.info("Rising HF - Battery | %s | %s ", eui, message)
+        battery = (int(message[16:], 16) + 150) * 0.01 / 3.64
+        sdlist.append(sensor_data(sd.time, sd.eui, float(battery), "%", 3))
+
+    elif (euis[eui] == "Digimondo"):
+        log.info("Digimondo - Power consumption | %s | %s ", eui, message)
+        # Decode power consumption
+        consumption = int(message[2:8], 16)
+        sdlist.append(sensor_data(sd.time, sd.eui, float(consumption), "kWh", 4))
+
+    elif (euis[eui] == "LS-11x"):
+        log.info("LS-11x - Temperature | %s | %s ", eui, message)
+        # Decoding temperature
+        temp = int(message[2:6], 16) / 100
+        sdlist.append(sensor_data(sd.time, sd.eui, float(temp), "°C", 1))
+
+        log.info("LS-11x - Humidity | %s | %s ", eui, message)
+        # Decoding temperature
+        hum = int(message[6:10], 16) / 100
+        sdlist.append(sensor_data(sd.time, sd.eui, float(hum), "%RH", 2))
+
+        log.info("LS-11x - Gas density | %s | %s ", eui, message)
+        # Decoding Gas density
+        dev_type = ("CO2", "CO", "PM2.5")
+        unit = dev_type[int(message[0:2], 16) - 1]
+        co2 = int(message[10:], 16)
+        sdlist.append(sensor_data(sd.time, sd.eui, float(co2), unit, 5))
+
+def write_sql(time, eui, value, unit, sensor_type_id):
+    # log.info("write_sql()")
+    sql_cmd = 'INSERT INTO ' + '\"' + sql['table'] + '\" (Time, \" EUI\",  \" Value\", \" Unit\", \"Sensor_type_ID\") VALUES (?, ?, ?, ?, ?)'
+    # log.info("SQL command: %s %s, %s, %s, %s, %s", sql_cmd, time, eui, value, unit, sensor_type_id)
+    cursor.execute(sql_cmd, time, eui, value, unit, sensor_type_id)
+    sql_conn.commit()
+
+def gettime():
+    #log.info("gettime()")
+    now = dt.datetime.now()
+    time = str(now.day).rjust(2, '0') + "/" + str(now.month).rjust(2, '0') + "/" + str(now.year).rjust(2, '0') + " " + str(now.hour).rjust(2, '0') + ":" + str(now.minute).rjust(2, '0')
+    return time
 
 def on_open(ws):
-    def run(*args):
-        for i in range(3):
-            time.sleep(1)
-            ws.send("Hello %d" % i)
-        time.sleep(1)
-        ws.close()
-        print("thread terminating...")
-    thread.start_new_thread(run, ())
+    log.info("Web Socket open")
+    
+def on_close(ws):
+    log.info("Web Socket closed")
+    exit(1)
+
+def on_error(ws, error):
+    log.info("Web Socket error")
+    print(error)
+
+def write_sql_info():
+    with open('sqlinfo.txt', 'w') as f:
+        f.write(json.dumps(sql))
+    f.close()
+
+def write_sensor_list():
+    with open('sensors.txt', 'w') as f:
+        f.write(json.dumps(euis))
+    f.close()
 
 if __name__ == "__main__":
-    websocket.enableTrace(True)
-    # ws = websocket.WebSocketApp("ws://echo.websocket.org/",
-    ws = websocket.WebSocketApp("wss://ah1.connectmedia.hu/app?token=vgEAOwAAABNhaDEuY29ubmVjdG1lZGlhLmh1YgQNgRJADuklUJ6GwoA06Q==",
+
+    log = logging.getLogger(__name__)
+    log.setLevel(logging.INFO)
+    logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%d/%m/%Y %H:%M')
+
+    # Read SQL connection data
+    with open('sqlinfo.txt', 'r') as f:
+        sql = json.loads(f.read())
+    f.close()
+
+    # Read eui list
+    with open('sensors.txt', 'r') as f:
+        euis = json.loads(f.read())
+    f.close()
+
+    sql_conn = odbc.connect(        \
+        'DRIVER={SQL Server};'
+        + 'SERVER=' + sql["server"] + ';'
+        + 'DATABASE=' + sql["database"] + ';'
+        + 'Trusted_Connection=yes;'
+    )
+    cursor = sql_conn.cursor()
+
+    # Read URL containing web socket app hash
+    f = open('app_url.txt', 'r')
+    app_url = f.readline()
+    f.close()
+
+    websocket.enableTrace(False)
+    ws = websocket.WebSocketApp(app_url,
                                 on_message = on_message,
                                 on_error = on_error,
                                 on_close = on_close)
