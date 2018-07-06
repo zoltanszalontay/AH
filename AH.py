7a8699a21dd7015dd94d38078e4531978bb8dae4
Szalontay Zoltán <SZAZ@ad.ifua.hu>
2018-07-06 14:40:22 +0200

    Initial commit

--- AH.py
+++ AH.py
@@ -7,8 +7,12 @@ import json
 import pyodbc as odbc
 from pushover import Pushover
 
-sql = {}    # SQL configuration (server, database, table)
-euis = {}   # Sensor ID (EUI) - Sensor type pairs
+sql = {}            # SQL configuration (server, database, table)
+euis = {}           # Sensor ID (EUI) - Sensor type pairs
+pushover_cfg = {}   # Pushover push notification service configuration
+
+sdlist = []
+prev_sdlist = []
 
 class sensor_data:
     def __init__(self, time, eui, value, unit, sensor_type_id, sensor_model):
@@ -19,32 +23,36 @@ class sensor_data:
         self.sensor_type_id = sensor_type_id
         self.sensor_model = sensor_model
 
-sdlist = []
 sd_temp = sensor_data 
 sd_power = sensor_data
 
 def on_message(ws, message):
     #log.info("on_message()")
-
     time = gettime()
-    
+
     jsondata = json.loads(message)
-    decode(time, jsondata["EUI"], jsondata["data"])
-    
-    # Insert sensor data to SQL table
-    for i in range(len(sdlist)):
-        log.info("|| %-30s | %-20s || %s, %s, %8.2f, %-4s, %2s ||" % (sdlist[i].sensor_model, jsondata["data"], sdlist[i].time, sdlist[i].eui, float(sdlist[i].value), sdlist[i].unit, sdlist[i].sensor_type_id))
-        write_sql(sdlist[i].time, sdlist[i].eui, round(float(sdlist[i].value), 2), sdlist[i].unit, sdlist[i].sensor_type_id)
+    print("jsondata: Command: %s EUI: %s data: %s " % (jsondata["cmd"], jsondata["EUI"], jsondata["data"]))
+
+    # Process rx messages only (drop gw messages)
+    if(jsondata["cmd"] == "rx"):
+        decode(time, jsondata["EUI"], jsondata["data"])
+        
+        # Insert sensor data to SQL table
+        for i in range(len(sdlist)):
+            log.info("|| %-30s | %-20s || %s, %s, %8.2f, %-4s, %2s ||" % (sdlist[i].sensor_model, jsondata["data"], sdlist[i].time, sdlist[i].eui, float(sdlist[i].value), sdlist[i].unit, sdlist[i].sensor_type_id))
+            write_sql(sdlist[i].time, sdlist[i].eui, round(float(sdlist[i].value), 2), sdlist[i].unit, sdlist[i].sensor_type_id)
+
+    return
 
 def decode(time, eui, message):
     #log.info("decode()")
-    
-    # Reinitialize sensor list
-    del sdlist[:]
 
+    # Add time and sensor ID (EUI)
     sd = sensor_data("", "", 0.0, "", "", "")
     sd.time = time
     sd.eui = eui
+
+    del sdlist[:]
     
     # Decode supported sensors only (Rising HF, Digimondo and LS-11x)
     if (euis[eui] == "Rising HF"):
@@ -62,7 +70,7 @@ def decode(time, eui, message):
         # log.info("Rising HF - Battery | %s | %s ", eui, message)
         battery = (int(message[16:], 16) + 150) * 0.01 / 3.64
         sdlist.append(sensor_data(sd.time, sd.eui, float(battery), "%", 3, "Rising HF - Battery"))
-
+        
     elif (euis[eui] == "Digimondo"):
         # log.info("Digimondo - Power consumption | %s | %s ", eui, message)
         # Decode power consumption
@@ -85,14 +93,20 @@ def decode(time, eui, message):
         dev_type = ("CO2", "CO", "PM2.5")
         unit = dev_type[int(message[0:2], 16) - 1]
         co2 = int(message[10:], 16)
+        # print("message: %s, msg co2: 0x%s, co2: %f" % (message, message[6:8], int(message[10:], 16)))
         sdlist.append(sensor_data(sd.time, sd.eui, float(co2), unit, 5, "LS-11x - Gas density"))
 
+    return sdlist
+
 def write_sql(time, eui, value, unit, sensor_type_id):
+    # For debug purposes
+    return
     # log.info("write_sql()")
     sql_cmd = 'INSERT INTO ' + '\"' + sql['table'] + '\" (Time, \" EUI\",  \" Value\", \" Unit\", \"Sensor_type_ID\") VALUES (?, ?, ?, ?, ?)'
     # log.info("SQL command: %s %s, %s, %s, %s, %s", sql_cmd, time, eui, value, unit, sensor_type_id)
     cursor.execute(sql_cmd, time, eui, value, unit, sensor_type_id)
     sql_conn.commit()
+    return
 
 def gettime():
     #log.info("gettime()")
@@ -103,31 +117,57 @@ def gettime():
 def on_open(ws):
     log.info("Web Socket open")
     push_notification("Web socket open")
-
+    return
+ 
 def on_close(ws):
     log.info("Web Socket closed")
     push_notification("Web socket closed")
-    exit(1)
+    # Reopen websocket
+    open_websocket()
+    # exit(1)
+    return
 
 def on_error(ws, error):
     log.info("Web Socket error")
     push_notification("Web socket error")
     print(error)
+    # Reopen websocket
+    open_websocket()
+    return
+
+def open_websocket():
+    websocket.enableTrace(False)
+    ws = websocket.WebSocketApp(app_url,
+                                on_message = on_message,
+                                on_error = on_error,
+                                on_close = on_close)
+    ws.on_open = on_open
+    ws.run_forever()
+    return
 
 def write_sql_info():
     with open('sqlinfo.txt', 'w') as f:
         f.write(json.dumps(sql))
     f.close()
+    return
 
 def write_sensor_list():
     with open('sensors.txt', 'w') as f:
         f.write(json.dumps(euis))
     f.close()
+    return
+
+def write_pushover_config():
+    with open('pushover.txt', 'w') as f:
+        f.write(json.dumps(pushover_cfg))
+    f.close()
+    return
 
 def push_notification(message):
     msg = po.msg(message)
     msg.set("AH IoT", "Data streamer")
     po.send(msg)
+    return
 
 if __name__ == "__main__":
 
@@ -136,8 +176,12 @@ if __name__ == "__main__":
     logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%d/%m/%Y %H:%M')
 
     # Start push notification
-    po = Pushover("aj2rn8589hxxcjj8aoqayoxp8cffg6") # application key
-    po.user("uenw7rc79czvecd3fsjrms9bm32ucm")       # user key
+    # Read Pushover configuration
+    with open('pushover.txt', 'r') as f:
+        pushover_cfg = json.loads(f.read())
+    f.close()
+    po = Pushover(pushover_cfg["application_key"])
+    po.user(pushover_cfg["user_key"])
 
     # Read SQL connection data
     with open('sqlinfo.txt', 'r') as f:
@@ -149,23 +193,17 @@ if __name__ == "__main__":
         euis = json.loads(f.read())
     f.close()
 
-    sql_conn = odbc.connect(        \
-        'DRIVER={SQL Server};'
-        + 'SERVER=' + sql["server"] + ';'
-        + 'DATABASE=' + sql["database"] + ';'
-        + 'Trusted_Connection=yes;'
-    )
-    cursor = sql_conn.cursor()
+    # sql_conn = odbc.connect(        \
+    #     'DRIVER={SQL Server};'
+    #     + 'SERVER=' + sql["server"] + ';'
+    #     + 'DATABASE=' + sql["database"] + ';'
+    #     + 'Trusted_Connection=yes;'
+    # )
+    # cursor = sql_conn.cursor()
 
     # Read URL containing web socket app hash
     f = open('app_url.txt', 'r')
     app_url = f.readline()
     f.close()
 
-    websocket.enableTrace(False)
-    ws = websocket.WebSocketApp(app_url,
-                                on_message = on_message,
-                                on_error = on_error,
-                                on_close = on_close)
-    ws.on_open = on_open
-    ws.run_forever()
+    open_websocket()
